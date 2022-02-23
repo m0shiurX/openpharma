686ee0aca3f13d757e0bd6a023386ba480a2a490
Moshiur Rahman <unimrgm@gmail.com>
2022-02-24 01:27:17 +0600

    added: medicine db

---
+++ database/seeders/MedicineSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MedicineSeeder extends Seeder
+{
+    public function run(): void
+    {
+        \App\Models\Medicine::factory(20)->create();
+    }
+}
