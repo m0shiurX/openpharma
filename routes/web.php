bec113aea9a228bc06cf46ca0911d4fd092391f6
Moshiur Rahman <unimrgm@gmail.com>
2022-02-25 15:38:46 +0600

    routes cleaned

--- routes/web.php
+++ routes/web.php
@@ -13,66 +13,37 @@ use App\Http\Controllers\ManufacturerController;
 // Auth Routes
 require __DIR__ . '/auth.php';
 
-// Default route will be redirected to login screen for unauthencated users
+// Default route will be redirected to login screen for unauthenticated users
 Route::redirect('/', '/dashboard');
 
-// Authenticated routes will go here
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('profile', [ProfileController::class, 'index'])
-    ->name('profile')
-    ->middleware('auth');
-
-Route::get('profile/edit', [ProfileController::class, 'create'])
-    ->name('profile.edit')
-    ->middleware('auth');
-
-Route::put('profile/update', [ProfileController::class, 'update'])
-    ->name('profile.update')
-    ->middleware('auth');
 
 
+Route::middleware('auth')->group(function () {
+
+    // Default route
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+
+    // Profile management
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('profile', 'index')->name('profile');
+        Route::get('profile/edit', 'create')->name('profile.edit');
+        Route::put('profile/update', 'update')->name('profile.update');
+    });
+
+
+    // Resources
+    Route::resource('stocks', StockController::class);
+    Route::resource('customers', CustomerController::class);
+    Route::resource('medicines', MedicineController::class);
+    Route::resource('purchases', PurchaseController::class);
+    Route::resource('manufacturers', ManufacturerController::class);
+});
 
 
 // Images
 Route::get('/img/{path}', [ImagesController::class, 'show'])
     ->where('path', '.*')
     ->name('image');
-
-
-
-// Manufacturers
-Route::get('manufacturers', [ManufacturerController::class, 'index'])
-    ->name('manufacturers.index')
-    ->middleware('auth');
-
-// Manufacturers
-Route::get('manufacturers/create', [ManufacturerController::class, 'create'])
-    ->name('manufacturers.create')
-    ->middleware('auth');
-Route::post('manufacturers/store', [ManufacturerController::class, 'store'])
-    ->name('manufacturers.store')
-    ->middleware('auth');
-
-Route::get('manufacturers/{manufacturer}', [ManufacturerController::class, 'show'])
-    ->name('manufacturers.show')
-    ->middleware('auth');
-
-Route::get('manufacturers/{manufacturer}/edit', [ManufacturerController::class, 'edit'])
-    ->name('manufacturers.edit')
-    ->middleware('auth');
-
-Route::put('manufacturers/{manufacturer}', [ManufacturerController::class, 'update'])
-    ->name('manufacturers.update')
-    ->middleware('auth');
-
-Route::delete('manufacturers/{manufacturer}', [ManufacturerController::class, 'destroy'])
-    ->name('manufacturers.destroy')
-    ->middleware('auth');
-
-Route::resource('customers', CustomerController::class);
-Route::resource('medicines', MedicineController::class);
-Route::resource('stocks', StockController::class);
-Route::resource('purchases', PurchaseController::class);
