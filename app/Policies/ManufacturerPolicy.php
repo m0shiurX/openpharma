b7bcf5abad218f0468303435f9b08d873f3c9000
Moshiur Rahman <unimrgm@gmail.com>
2022-03-02 02:00:20 +0600

    cleanup: returns and comments

--- app/Policies/ManufacturerPolicy.php
+++ app/Policies/ManufacturerPolicy.php
@@ -10,83 +10,42 @@ class ManufacturerPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
+
     public function viewAny(User $user)
     {
         //
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Manufacturer  $manufacturer
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function view(User $user, Manufacturer $manufacturer)
     {
         //
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
+
     public function create(User $user)
     {
         //
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Manufacturer  $manufacturer
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
+
     public function update(User $user, Manufacturer $manufacturer)
     {
         //
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Manufacturer  $manufacturer
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
+
     public function delete(User $user, Manufacturer $manufacturer)
     {
         //
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Manufacturer  $manufacturer
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
+
     public function restore(User $user, Manufacturer $manufacturer)
     {
         //
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Manufacturer  $manufacturer
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
+
     public function forceDelete(User $user, Manufacturer $manufacturer)
     {
         //
