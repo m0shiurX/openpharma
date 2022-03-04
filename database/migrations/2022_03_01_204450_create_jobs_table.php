aa8fca5e176ae9dfa33f648d9c423dfb029db54f
Moshiur Rahman <unimrgm@gmail.com>
2022-03-04 18:58:27 +0600

    updated jobs

--- database/migrations/2022_03_01_204450_create_jobs_table.php
+++ database/migrations/2022_03_01_204450_create_jobs_table.php
@@ -6,13 +6,9 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
+        Schema::dropIfExists('jobs');
         Schema::create('jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('queue')->index();
@@ -24,12 +20,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('jobs');
     }
