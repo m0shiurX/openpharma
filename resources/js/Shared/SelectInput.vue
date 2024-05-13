cf6ca247cc399db7542b00b02a2311be1a418e08
Moshiur Rahman <unimrgm@gmail.com>
2024-05-13 15:45:19 +0600

    updated

--- resources/js/Shared/SelectInput.vue
+++ resources/js/Shared/SelectInput.vue
@@ -1,47 +1,47 @@
 <template>
-  <div :class="$attrs.class">
-    <label v-if="label" class="form-label" :for="id">{{ label }}:</label>
-    <select :id="id" ref="input" v-model="selected" v-bind="{ ...$attrs, class: null }" class="form-select" :class="{ error: error }">
-      <slot />
-    </select>
-    <div v-if="error" class="form-error">{{ error }}</div>
-  </div>
+    <div :class="$attrs.class">
+        <label v-if="label" class="form-label" :for="id">{{ label }}:</label>
+        <select :id="id" ref="input" v-model="selected" v-bind="{ ...$attrs, class: null }" class="form-select" :class="{ error: error }">
+            <slot />
+        </select>
+        <div v-if="error" class="form-error">{{ error }}</div>
+    </div>
 </template>
 
 <script>
-import { v4 as uuid } from 'uuid'
+import { v4 as uuid } from 'uuid';
 
 export default {
-  inheritAttrs: false,
-  props: {
-    id: {
-      type: String,
-      default() {
-        return `select-input-${uuid()}`
-      },
+    inheritAttrs: false,
+    props: {
+        id: {
+            type: String,
+            default() {
+                return `select-input-${uuid()}`;
+            },
+        },
+        error: String,
+        label: String,
+        modelValue: [String, Number, Boolean],
     },
-    error: String,
-    label: String,
-    modelValue: [String, Number, Boolean],
-  },
-  emits: ['update:modelValue'],
-  data() {
-    return {
-      selected: this.modelValue,
-    }
-  },
-  watch: {
-    selected(selected) {
-      this.$emit('update:modelValue', selected)
+    emits: ['update:modelValue'],
+    data() {
+        return {
+            selected: this.modelValue,
+        };
     },
-  },
-  methods: {
-    focus() {
-      this.$refs.input.focus()
+    watch: {
+        selected(selected) {
+            this.$emit('update:modelValue', selected);
+        },
     },
-    select() {
-      this.$refs.input.select()
+    methods: {
+        focus() {
+            this.$refs.input.focus();
+        },
+        select() {
+            this.$refs.input.select();
+        },
     },
-  },
-}
+};
 </script>
